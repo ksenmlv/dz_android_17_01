205440a63aa87302d4dc4ff65bf64bc0b2236d42
ksenia <ksusamaleeva05@gmail.com>
2024-01-16 23:08:55 +0300

    first commit

---
+++ app/src/main/java/com/example/myfirstapp/text_on_imageActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class text_on_imageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_on_image);
+    }
+}
---
+++ app/src/main/java/text_on_imageActivity.java
@@ -0,0 +1,19 @@
+//import android.os.Bundle;
+//import androidx.appcompat.app.AppCompatActivity;
+//import com.example.myfirstapp.R;
+//
+//public class text_on_imageActivity {
+////    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.text_on_image);
+//    }
+//}
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myfirstapp.R;
+
+public class text_on_imageActivity extends AppCompatActivity {
+}
